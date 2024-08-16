d352aa4b25a6e43e4a0a79ea0834a84fe20ed4bd
Vivek <vs690799@gmail.com>
2024-08-16 22:14:38 +0530

    pass stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,7 +1,10 @@
+#include <cstdlib>
 #include <iostream>
 #include <filesystem>
 #include <fstream>
 #include <string>
+#include <zlib.h>
+
 
 int main(int argc, char *argv[])
 {
@@ -41,7 +44,42 @@ int main(int argc, char *argv[])
             std::cerr << e.what() << '\n';
             return EXIT_FAILURE;
         }
-    } else {
+    } else if (command == "cat-file") {
+        std::string flag = argv[2];
+        if(argc <  4 || std::string(argv[2]) != "p" ) {
+            std::cerr << "Missing paramerter: -p <hash>\n";
+            return  EXIT_FAILURE;
+        }
+        const auto blob_hash = std::string_view(argv[3], 40);
+                const auto blob_dir = blob_hash.substr(0, 2);
+                const auto blob_name = blob_hash.substr(2);
+
+                const auto blob_path = std::filesystem::path(".git") / "objects" / blob_dir / blob_name;
+                auto in = std::ifstream(blob_path);
+                if (!in.is_open()) {
+                    std::cerr << "Failed to open " << blob_path << " file.\n";
+                    return EXIT_FAILURE;
+                }
+                const auto blob_data = std::string(std::istreambuf_iterator<char>(in), std::istreambuf_iterator<char>());
+                auto buf = std::string();
+                buf.resize(blob_data.size());
+                while (true) {
+                    auto len = buf.size();
+                    if (auto res = uncompress((uint8_t*)buf.data(), &len, (const uint8_t*)blob_data.data(), blob_data.size()); res == Z_BUF_ERROR) {
+                        buf.resize(buf.size() * 2);
+                    } else if (res != Z_OK) {
+                        std::cerr << "Failed to uncompress Zlib. (code: " << res << ")\n";
+                        return EXIT_FAILURE;
+                    } else {
+                        buf.resize(len);
+                        break;
+                    }
+                }
+                std::cout << std::string_view(buf).substr(buf.find('\0') + 1);
+
+    }
+
+    else {
         std::cerr << "Unknown command " << command << '\n';
         return EXIT_FAILURE;
     }
