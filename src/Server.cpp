3708647c36f35d82b115f9321fc7eabef8fd88a5
Vivek <vs690799@gmail.com>
2024-08-16 22:24:41 +0530

    pass stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -13,7 +13,6 @@ int main(int argc, char *argv[])
     std::cerr << std::unitbuf;
 
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cout << "Logs from your program will appear here!\n";
 
     // Uncomment this block to pass the first stage
 
